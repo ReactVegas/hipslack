a9fa2f906a04286edfacbabd41cc88044c65daf5
Fred Guest <fredguest@mac.com>
2015-10-08 10:26:10 -0700

    control the current user input

--- client/components/CurrentUserInput.js
+++ client/components/CurrentUserInput.js
@@ -1,5 +1,8 @@
-import { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Component, PropTypes } from 'react';
 import { INPUT_STYLES, BUTTON_STYLES } from 'constants/styles';
+import { setCurrentUser } from 'actions/ui';
 
 const styles = {
   title: {
@@ -15,13 +18,25 @@ const styles = {
 };
 
 class CurrentUserInput extends Component {
+  static propTypes = {
+    currentUser: PropTypes.string.isRequired,
+    setCurrentUser: PropTypes.func.isRequired
+  }
+
   render() {
+    const { currentUser, setCurrentUser } = this.props;
     return (
       <div>
         <div style={styles.title}>
           Join Hipslack so you can chat!
         </div>
-        <input style={styles.input} placeholder="Enter your name..." autoFocus={true}/>
+        <input
+          autoFocus={true}
+          style={styles.input}
+          placeholder="Enter your name..."
+          onChange={(event) => setCurrentUser(event.target.value)}
+          value={currentUser}
+        />
         <button style={styles.button} onClick={::this.handleClick}>
           Join Hipslack!
         </button>
@@ -34,4 +49,16 @@ class CurrentUserInput extends Component {
   }
 }
 
-export default CurrentUserInput;
+function select(state) {
+  return {
+    currentUser: state.ui.currentUser
+  };
+}
+
+function actions(dispatch) {
+  return bindActionCreators({
+    setCurrentUser: setCurrentUser
+  }, dispatch);
+}
+
+export default connect(select, actions)(CurrentUserInput);
